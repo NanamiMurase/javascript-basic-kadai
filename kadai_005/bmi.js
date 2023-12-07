33bf4b484ff95e129d73191632c01671b1048b90
NanamiMurase <149807149+NanamiMurase@users.noreply.github.com>
2023-12-07 14:52:14 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,22 @@
+ // 変数の宣言
+ let bodyWeight;
+
+ // 値の代入
+bodyWeight = '68';
+
+ // 変数の宣言
+ let bodyHeight;
+
+// 値の代入
+bodyHeight = 1.7;
+
+ // 変数の宣言
+ let bmiNumber;
+
+// 値の代入
+bmiNumber = (bodyWeight / (bodyHeight * bodyHeight));
+
+console.log(bmiNumber);
+
+
+
