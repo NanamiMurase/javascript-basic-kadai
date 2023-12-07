9254df5e0a73dc5299051deacbb0bda9170f7113
NanamiMurase <149807149+NanamiMurase@users.noreply.github.com>
2023-12-08 00:31:49 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,81 @@
+ // 変数の宣言
+ let num;
+
+    // 値の代入 3の倍数
+    num = 9;
+// 変数numの値を出力する（確認用）
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+ else if (num % 5 === 0) {
+   console.log('5の倍数です');
+ }
+else {
+  console.log(num);
+}
+
+  // 値の代入 5の倍数
+  num = 50;
+  // 変数numの値を出力する（確認用）
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+ else if (num % 5 === 0) {
+   console.log('5の倍数です');
+ }
+else {
+  console.log(num);
+}
+
+  // 値の代入 3と5の倍数
+  num = 15;
+  // 変数numの値を出力する（確認用）
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+ else if (num % 5 === 0) {
+   console.log('5の倍数です');
+ }
+else {
+  console.log(num);
+}
+
+  // 値の代入 (num)を出力
+  num = 2;
+  // 変数numの値を出力する（確認用）
+console.log(num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です');
+}
+else if (num % 3 === 0) {
+  console.log('3の倍数です');
+}
+ else if (num % 5 === 0) {
+   console.log('5の倍数です');
+ }
+else {
+  console.log(num);
+}
+
+
+
+
+
+
+
